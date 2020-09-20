db8f42098e844f5bc077659530e41e7b89a7951a
Sergio Marelli <sergio.mrll@gmail.com>
2020-09-20 23:07:27 +0100

    added logout and some authentication helpers also solved some issues with packages to make DELETE methods work

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 	get '/signup', to: 'users#new'
 	post '/signup', to: 'users#create'
 
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+
+
   resources :cafes
   resources :notes
   resources :users
