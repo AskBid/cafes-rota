586a70ddd7d723a557aa5b21fbc7a9812ac154e2
Sergio Marelli <sergio.mrll@gmail.com>
2020-09-20 11:37:51 +0100

    working on the signup form and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,25 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		# binding.pry
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			redirect_to user_path(user)
+		else
+			# binding.pry
+			redirect_to new_user_path
+		end 
+	end
+
+	def show
+		render text: '<h1>User Show</h1>'
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
 	end
 
 end
