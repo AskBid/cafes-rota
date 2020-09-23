eeb2f0e1ab1b507231200abd7cc0a875c66f91cf
Sergio Marelli <sergio.mrll@gmail.com>
2020-09-23 21:53:07 +0100

    login session#create created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,12 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(name: params[:user][:name]) || User.find_by(email: params[:user][:email])
-		binding.pry
+		if user && user.authenticate(params[:user][:password])
+			session[:user_id] = user.id
+			redirect_to user
+		else
+			flash[:message] = 'You need to Signup or perhaps you entered wrong credentials? Try again!'
+			redirect_to login_path
+		end
 	end
 end
