e6fc19bef3c471d65bcbf638eb2bd049e43fa2fb
Sergio Marelli <sergio.mrll@gmail.com>
2020-09-24 23:04:40 +0100

    redone the omniauth-google with 2.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 	#new happens automagically
 
 	def create
+		binding.pry
 		user = User.find_by(name: params[:user][:name]) || User.find_by(email: params[:user][:email])
 		if user && user.authenticate(params[:user][:password])
 			session[:user_id] = user.id
