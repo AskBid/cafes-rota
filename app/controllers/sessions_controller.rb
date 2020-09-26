7b57147ac3cbedda9ec159867d9b9c447c25dae2
Sergio Marelli <sergio.mrll@gmail.com>
2020-09-26 17:13:39 +0100

    typo in omnia login fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
 	#new happens automagically
 
 	def create
-		binding.pry
 		user = User.find_by(name: session_params[:name]) || User.find_by(email: session_params[:email])
 		if user && user.authenticate(session_params[:password])
 			session[:user_id] = user.id
@@ -22,7 +21,7 @@ class SessionsController < ApplicationController
 		user = User.from_omniauth(auth)
 		binding.pry #User method to go here
 		if user.valid?
-			session[:uder_id] = user.id
+			session[:user_id] = user.id
 			redirect_to user
 		else
 			redirect_to login_path
