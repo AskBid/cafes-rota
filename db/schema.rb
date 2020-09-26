26acffe5739bc534da11331201643fe77fc7f417
Sergio Marelli <sergio.mrll@gmail.com>
2020-09-26 15:06:34 +0100

    added :uid and :provider to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_112638) do
+ActiveRecord::Schema.define(version: 2020_09_26_140330) do
 
   create_table "cafes", force: :cascade do |t|
     t.string "name"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2020_09_12_112638) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
 end
