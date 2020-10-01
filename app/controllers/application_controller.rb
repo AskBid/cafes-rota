fcd8fb0b83f8266579f66c953c2e39cd4e1d211e
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-01 10:51:59 +0100

    moved application_helper to application_controller with the use of helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+	#gives access to the methods in views
+	helper_method :user_is_authenticated, :current_user
+
 	def home
 	end
+
+	private
+
+	def user_is_authenticated
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-	def user_is_authenticated
-    !!current_user
-  end
-
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
 end
