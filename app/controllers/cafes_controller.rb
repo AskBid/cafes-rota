6a2dd65c1c07f1e2e9ec4b773885b0303c17eaa4
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-02 19:11:11 +0100

    initialised cafes#create and cafe_params

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -8,4 +8,15 @@ class CafesController < ApplicationController
 		@cafe.links.build(name: 'website')
 		@cafe.populate_new_cafe
 	end
+
+	def create
+		cafe = Cafe.new(cafe_params)
+		binding.pry
+	end
+
+	private
+
+	def cafe_params
+		params.require(:cafe).permit(links_attributes: [:url])
+	end
 end
