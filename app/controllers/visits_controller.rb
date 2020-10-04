fed0a29d3483fc7cfc205c881963c0002d8d580d
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-04 13:44:06 +0100

    working on visits#create simplified the way params are given to this action and simplified teh partial layout for it

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,22 +1,36 @@
 class VisitsController < ApplicationController
   def index
-  	@user = User.find_by_slug(user_params[:user_slug])
+    this_user
   	@visits = @user.visits
   end
 
   def new
-    user = User.find_by_slug(user_params[:user_slug])
-  	@cafes = user.missing_cafes
+    this_user
+  	@cafes = @user.missing_cafes
   end
 
   def create
-    # binding.pry
-    redirect_to new_user_visit_path(current_user.slug)
+    this_user
+    @user.cafe_ids << visit_params[:cafe_id]
+    binding.pry
+    redirect_to new_user_visit_path(@user.slug)
+  end
+
+  def update
+    binding.pry
   end
 
   private
 
+  def this_user
+    @user = User.find_by_slug(user_params[:user_slug])
+  end 
+
   def user_params
   	params.permit(:user_slug)
+  end
+
+  def visit_params
+    params.require(:visit).permit(:cafe_id)
   end
 end
