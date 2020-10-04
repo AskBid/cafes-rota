a790f5ef1d8ebb02ce4fcdd61716ee14eb3e87f5
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-04 22:04:56 +0100

    opening validation

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -1,3 +1,6 @@
 class Opening < ApplicationRecord
 	belongs_to :cafe
+
+	validates :day, inclusion: { in: %w(monday tuesday wednesday thursday friday saturday sunday),
+    message: "%{value} is not a valid day of the week" }
 end
