9a6f1609f900f425c29ec20e7609f469383ca282
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-08 14:50:59 +0100

    adding info to cafes/show view

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,4 +3,5 @@ class Visit < ApplicationRecord
 	belongs_to :cafe
 
 	scope :by_user_today, -> (user_id, todays_date) {where("user_id = ? AND last_visited = ?", user_id, todays_date) }
+	scope :by_user_and_cafe, -> (user, cafe) {where("user_id = ? AND cafe_id = ?", user.id, cafe.id)}
 end
