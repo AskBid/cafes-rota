6e72b319fe8b018e8e9c0e8a606d3ecb0c8c0b53
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-11 19:34:01 +0100

    prepared cafes#index controller and notes scopes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,13 @@
 class NotesController < ApplicationController
 	def index
+		@cafe = Cafe.find_by_slug(params[:cafe_slug])
+		@notes = Note.by_cafe(@cafe)
+		@user_notes = Note.by_cafe(@cafe).by_user(@current_user) if user_is_authenticated
 	end
 
 	def new
 	end
-	
+
 	def create
 	end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,7 @@
 class Note < ApplicationRecord
 	belongs_to :user
 	belongs_to :cafe
+
+	scope :by_cafe, -> (cafe) {where('cafe_id = ?', cafe.id)}
+	scope :by_user, -> (user) {where('user_id = ?', user.id)}
 end
