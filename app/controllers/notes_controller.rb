9a645d759780d18350b8de93dc520f0196812535
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-11 19:50:38 +0100

    notes#new controller and some fixing

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,23 @@
 class NotesController < ApplicationController
 	def index
-		@cafe = Cafe.find_by_slug(params[:cafe_slug])
+		@cafe = this_cafe
 		@notes = Note.by_cafe(@cafe)
-		@user_notes = Note.by_cafe(@cafe).by_user(@current_user) if user_is_authenticated
+		@user_notes = Note.by_cafe(@cafe).by_user(current_user) if user_is_authenticated
 	end
 
 	def new
+		@cafe = this_cafe
+		binding.pry
+		@note = @cafe.notes.build
+		@note.user = current_user
 	end
 
 	def create
+	end
+
+	private
+
+	def this_cafe
+		Cafe.find_by_slug(params[:cafe_slug])
 	end
 end
