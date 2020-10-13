77ae8312519a8a693711b1e50b9604f2c90f4b4a
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-13 19:12:22 +0100

    notes view improvements and validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,8 @@ class Note < ApplicationRecord
 	belongs_to :user
 	belongs_to :cafe
 
+	validates_length_of :text, :maximum => 180
+
 	scope :by_cafe, -> (cafe) {where('cafe_id = ?', cafe.id)}
 	scope :by_user, -> (user) {where('user_id = ?', user.id)}
 end
